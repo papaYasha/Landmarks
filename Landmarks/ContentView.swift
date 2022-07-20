20a475588db3d0ddc42565767563bffc9be5622f
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-07-20 23:39:58 +0300

    1 section

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Macbook on 20.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
