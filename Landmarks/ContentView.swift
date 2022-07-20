85a64e7621d419f6edcdec46152a32d39f5e9a57
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-07-21 00:17:52 +0300

    Section 3

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-            .foregroundColor(.black)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.black)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
